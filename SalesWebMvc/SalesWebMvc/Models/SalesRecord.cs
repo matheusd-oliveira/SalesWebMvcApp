d40895b0a47e7b01bc002a3d9a4ed061c7bbac8a
Matheus Oliveira <matheussilvaolveira@gmail.com>
2023-03-16 00:15:45 -0300

    Add local project

---
+++ SalesWebMvc/SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using SalesWebMvc.Models.Enums;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        /// <summary>
+        /// SalesRecord = Registro de Vendas que vai ter um vendedor especifico para essa venda.
+        /// Lembrar que 1 vendedor pode ter mais de uma SalesRecord , ou seja, mais de uma venda.
+        /// Amount = Quantidade de vendas.
+        /// </summary>
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+
+        public SalesRecord()
+        {
+        }
+
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
---
+++ SalesWebMvc/SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,24 @@
+﻿using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        // GET ALL SELLERS 
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
